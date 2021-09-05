bae74836afd98d2b7196d9b8f1d7a518ba184f81
liitlelenim <jakubblaszczyk20@gmail.com>
2021-09-05 19:27:48 +0200

    create basic user model

---
+++ src/main/java/me/littlelenim/simplestudentteachercrud/model/Student.java
@@ -0,0 +1,43 @@
+package me.littlelenim.simplestudentteachercrud.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "student")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student
+{
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+    @Id
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @OneToMany(
+            fetch = FetchType.EAGER,
+            cascade = CascadeType.ALL
+
+    )
+    @JoinColumn(
+            name = "grades_id",
+            referencedColumnName = "id"
+    )
+    private List<Grade> grades;
+}
