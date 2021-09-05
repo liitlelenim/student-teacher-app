e6a1d786426a675ff5ae877b4ee2c540b72f8003
liitlelenim <jakubblaszczyk20@gmail.com>
2021-09-05 18:58:35 +0200

    initial commit

---
+++ src/main/java/me/littlelenim/simplestudentteachercrud/SimpleStudentTeacherCrudApplication.java
@@ -0,0 +1,13 @@
+package me.littlelenim.simplestudentteachercrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleStudentTeacherCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleStudentTeacherCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/littlelenim/simplestudentteachercrud/SimpleStudentTeacherCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package me.littlelenim.simplestudentteachercrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleStudentTeacherCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
