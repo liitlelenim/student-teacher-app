ad6cda197725645ac524a129ab6208432e3d105b
liitlelenim <jakubblaszczyk20@gmail.com>
2021-09-12 08:14:14 +0200

    create grade component

---
+++ src/frontend/src/component/app-main/student-table/grade/Grade.jsx
@@ -0,0 +1,21 @@
+import "./Grade.css"
+import {Popover} from "react-bootstrap";
+import {OverlayTrigger} from "react-bootstrap";
+
+const Grade = ({grade}) =>{
+    const descriptionPopover = (
+        <Popover className={"grade-popover"}>
+            <Popover.Header className={"grade-popover-header"}>
+                Weight: {grade.weight}
+            </Popover.Header>
+            <Popover.Body>
+                {grade.description}
+            </Popover.Body>
+        </Popover>
+    )
+
+    return <OverlayTrigger trigger={"hover"} placement={"bottom"} overlay={descriptionPopover} >
+        <div className={"grade"}>5</div>
+    </OverlayTrigger>
+}
+export {Grade}
