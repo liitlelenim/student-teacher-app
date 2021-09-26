c3dbec31d704e030eec0e959ec80cf0e3822c4f4
liitlelenim <jakubblaszczyk20@gmail.com>
2021-09-26 08:59:03 +0200

    change InvalidIdException response status code

--- src/main/java/me/littlelenim/simplestudentteachercrud/exception/InvalidIdException.java
+++ src/main/java/me/littlelenim/simplestudentteachercrud/exception/InvalidIdException.java
@@ -1,11 +1,10 @@
 package me.littlelenim.simplestudentteachercrud.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.BAD_REQUEST)
-public class InvalidIdException  extends RuntimeException{
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
+public class InvalidIdException extends RuntimeException {
     public InvalidIdException(String errorMessage) {
         super(errorMessage);
     }
