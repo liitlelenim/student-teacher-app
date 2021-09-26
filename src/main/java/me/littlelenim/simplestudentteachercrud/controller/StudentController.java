ed5f70b9219b149fa10e7c6399c3106bb7dfcd89
liitlelenim <jakubblaszczyk20@gmail.com>
2021-09-26 10:13:31 +0200

    add relevant status codes to post endpoints

--- src/main/java/me/littlelenim/simplestudentteachercrud/controller/StudentController.java
+++ src/main/java/me/littlelenim/simplestudentteachercrud/controller/StudentController.java
@@ -1,14 +1,18 @@
 package me.littlelenim.simplestudentteachercrud.controller;
 
+import com.sun.net.httpserver.Authenticator;
 import lombok.AllArgsConstructor;
 import me.littlelenim.simplestudentteachercrud.dto.GradePostDTO;
 import me.littlelenim.simplestudentteachercrud.dto.StudentPatchDTO;
 import me.littlelenim.simplestudentteachercrud.dto.StudentPostDTO;
 import me.littlelenim.simplestudentteachercrud.model.Student;
 import me.littlelenim.simplestudentteachercrud.service.StudentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.net.http.HttpResponse;
 import java.util.List;
 
 @CrossOrigin
@@ -29,11 +33,13 @@ public class StudentController {
         return studentService.getById(id);
     }
 
+    @ResponseStatus(code = HttpStatus.CREATED)
     @PostMapping(value = "/{id}/grades")
     public void addGradeToStudent(@RequestBody @Valid GradePostDTO gradePostDTO, @PathVariable Long id) {
         studentService.addGradeByStudentId(id, gradePostDTO);
     }
 
+    @ResponseStatus(code = HttpStatus.CREATED)
     @PostMapping
     public void createStudent(@RequestBody @Valid StudentPostDTO studentPostDTO) {
         studentService.add(studentPostDTO);
